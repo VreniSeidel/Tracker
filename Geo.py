5dc75010c12efbd094fa2d42af19b90fbb8f7831
Vreni <vreniseidel492@gmail.com>
2021-09-14 12:26:01 +0200

    demo

--- Geo.py
+++ Geo.py
@@ -3,18 +3,41 @@ import ipaddress
 
 class Geo():
     def __init__(self):
-        self.ip = ipaddress.IPv4Address("192.168.1.1")
-        self.response = requests.get(f"https://geolocation-db.com/json/{self.ip}&position=true").json()
+
+        try:
+
+            self.ip = ipaddress.IPv4Address("74.195.1.128")
+            self.response = requests.get(f"https://geolocation-db.com/json/{self.ip}&position=true").json()
+            self.result_type_ip = self.check_public_private()
+
+        except(ConnectionError):
+            print("Check your internet connection")
 
 
     """Check public or private ip address"""
     def check_public_private(self):
         if self.ip.is_private:
-            print("Private")
-        else:
-            print("Public")
+            return "Private"
+        return "Public"
 
 
+    """{
+        'country_code': 'Not found', 
+        'country_name': 'Not found', 
+        'city': 'Not found', 
+        'postal': 'Not found', 
+        'latitude': 'Not found', 
+        'longitude': 'Not found', 
+        'IPv4': 'Not found', 
+        'state': 'Not found'
+    }"""
     """Information about ip address"""
     def information_ip(self):
-        print(self.response)
+        print(f"Country code: {self.response['country_code']}")
+        print(f"Country name: {self.response['country_name']}")
+        print(f"City: {self.response['city']}")
+        print(f"Postal: {self.response['postal']}")
+        print(f"Latitude: {self.response['latitude']}")
+        print(f"Longitude: {self.response['longitude']}")
+        print(f"IPv4: {self.response['IPv4']}")
+        print(f"State: {self.response['state']}")
